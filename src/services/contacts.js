d60cbfd33e98ad1e095969083036f2c1afdf9245
Yevheniiaa11 <yevheniia.zb@gmail.com>
2025-09-02 12:10:37 +0300

    services getallcontact

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -13,7 +13,7 @@ export const getAllContacts = async ({
   const limit = perPage;
   const skip = (page - 1) * perPage;
 
-  const contactsQuery = ContactsCollection.find(userId);
+  const contactsQuery = ContactsCollection.find({ userId });
 
   if (filter.contactType) {
     contactsQuery.where('contactType').equals(filter.contactType);
